99595ccb58a3525121550b146adb144e6f7cfc02
seangritthy <seangritthy@users.noreply.github.com>
2026-08-09 16:49:04 +0700

    release: v1.0.8 - remove external server.url to serve local APK bundle

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.seangritthy.tmdbvideo',
   appName: 'TMDB Video',
-  webDir: 'public',
-  server: {
-    url: 'https://vdomov.com',
-    cleartext: true
-  }
+  webDir: 'public'
 };
 
 export default config;
