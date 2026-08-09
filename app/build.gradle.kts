65b2833ac37173142ae00f3f5ba7b759927bed79
seangritthy <seangritthy@users.noreply.github.com>
2026-08-09 15:12:06 +0700

    release: v1.0.14 - add built-in fullscreen player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bongbee.tdmb"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
